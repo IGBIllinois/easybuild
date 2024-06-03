c6a544d2977f3260423f4ddb781cc806eb5bf2a2
Easy Build <easybuild@igb.illinois.edu>
2024-06-03 15:47:16 -0500

    Update antismash added bandage updated cURL Update GeneMark-ES Update gfatools Update HMMER Update Interproscan Update MultiQC Updated minimap2 Youdate PyTorch Update prokka Update R

--- easyconfigs/r/R/R_install_packages.R
+++ easyconfigs/r/R/R_install_packages.R
@@ -1,4 +1,4 @@
 install.packages("BiocManager")
-x <- setdiff(dir(path = "/home/apps/software/R/4.2.2-IGB-gcc-8.2.0/lib64/R/library"), dir(path = "/home/apps/software/R/4.3.2-IGB-gcc-8.2.0/lib64/R/library"))
+x <- setdiff(dir(path = "/home/apps/software/R/4.3.2-IGB-gcc-8.2.0/lib64/R/library"), dir(path = "/home/apps/software/R/4.4.0-IGB-gcc-8.2.0/lib64/R/library"))
 BiocManager::install(x)
 
